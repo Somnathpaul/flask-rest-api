d7e9be0fab5c6ac60caede41df9407d8e715db3e
Somnath Paul <Somnath4x@gmail.com>
2020-08-04 21:02:28 +0530

    modified route

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from markupsafe import escape
+
 import os
 
 # app
@@ -10,6 +12,14 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def home():
     return jsonify( {'name:':'flask' } )
+
+@app.route('/about', methods=['GET'])
+def about():
+    return 'Web application REST API made with flask!'
+
+@app.route('/user/<username>')
+def user(username):
+    return 'User %s' % escape(username)
 
 # server
 if __name__ == "__main__":
